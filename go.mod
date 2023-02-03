941ac2e499ac32fac34c8f9804d7816e4cf89644
arieltraver <arieltraver@users.noreply.github.com>
2023-02-03 14:56:27 -0500

    initwzeros go

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/arieltraver/ML_projects
 
 go 1.19
 
-require gonum.org/v1/gonum v0.12.0
+require (
+	gonum.org/v1/gonum v0.12.0
+	gonum.org/v1/hdf5 v0.0.0-20210714002203-8c5d23bc6946
+)
--- logreg.go
+++ logreg.go
@@ -1,3 +1,27 @@
 package main
-import "gonum.org/v1/gonum"
 
+import (
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/hdf5"
+	"fmt"
+)
+
+/**
+def initialize_w_zeroes(dim): #for most networks you would init with random values
+    zeroes = np.zeros((dim, 1))
+    b = 0
+    assert(zeroes.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int)) #i don't like python.
+    return zeroes, b
+
+**/
+
+func initWZeros(dim int) *mat.VecDense {
+	zeroSlice := make([]float64, dim) //initialize a slice filled with zeros
+	initWeights := mat.NewVecDense(dim, zeroSlice)
+	return initWeights
+}
+
+func main() {
+	fmt.Println(initWZeros(6))
+}
